3506e07c15dc1b57daaa7651cb7ed94a0fb71f8c
AndresLopez13 <andresilopeza@gmail.com>
2022-06-03 01:16:04 -0500

    fix required in navbar

--- PHP + Mongo/client/pages/about.php
+++ PHP + Mongo/client/pages/about.php
@@ -19,24 +19,26 @@
 
     <br><br><br><br>
     <!-- <h3>temas tecnicos y se explica mas de como funciona la pagina</h3> -->
-    <div class="about-class">
-    <h3 class="title">Acerca de</h3>
-    <?php include '../components/presentation.php'; ?>
-    </div>
-    <br><br>
-    <div>
-        <?php include "../components/footer.php"; ?>
-    </div>
-    <!-- Optional JavaScript; choose one of the two! -->
+    <div class="background">
+      <div class="about-class">
+      <h3 class="title">Acerca de</h3>
+      <?php include '../components/presentation.php'; ?>
+      </div>
+      <br><br>
+      <div>
+          <?php include "../components/footer.php"; ?>
+      </div>
+      <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <!-- <script src="../js/about.js"> </script> -->
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <!-- <script src="../js/about.js"> </script> -->
+      <!-- Option 2: Separate Popper and Bootstrap JS -->
+      <!--
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+      -->
+    </div>
   </body>
 </html>
 
