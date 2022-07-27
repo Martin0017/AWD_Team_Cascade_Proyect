4590d485e0e95a28abac0d99b41af718136504d1
DM2V <marcelo.malte11@gmail.com>
2022-07-26 21:23:38 -0500

    refactor REST

---
+++ Code/Unit 2/Rest Services/Team1Rest/reservaChoco.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express()
+const port = 3001;
+const mongoose = require('mongoose');
+const aboutDataRouter = require('./routes/aboutDataRoutes');
+const faunaRouter = require('./routes/faunaRoutes');
+const urlMongo='mongodb+srv://TeamCascade:123@clusterteamcascade.zjrq3.mongodb.net/InfoWeb?retryWrites=true&w=majority';
+
+mongoose.connect(urlMongo, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json());
+
+app.use('/ChocoAndino', aboutDataRouter);
+app.use('/ChocoAndino', faunaRouter);
+app.use('/ChocoAndino', faunaRouter);
+app.use('/ChocoAndino', faunaRouter);
+
+app.listen(port, () => console.log('Server Started on port ' + port))
